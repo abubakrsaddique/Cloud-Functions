b71eeeb320e2ee82eb340ad8fd1768f7300b717f
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-08-29 12:34:47 +0500

    SideBar Created

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,23 +2,49 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <nav className="bg-gray-800 p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <div className="text-white text-lg font-semibold">MyApp</div>
-          <div>
-            <Link href="/login">
-              <p className="text-white px-4 py-2 hover:bg-gray-700 rounded">
-                Login
-              </p>
-            </Link>
-            <Link href="/signup">
-              <p className="text-white px-4 py-2 hover:bg-gray-700 rounded">
-                Signup
-              </p>
-            </Link>
-          </div>
+    <div className="background min-h-screen h-full w-full">
+      <nav className="flex relative   w-full h-[74px] items-center justify-between px-[50px]  py-4">
+        <div>
+          <p className="text-3xl font-bold leading-[45px] text-white cursor-pointer">
+            ADMIN PANEL
+          </p>
         </div>
+        <div className="flex justify-center items-center cursor-pointer">
+          <Link href="/">
+            <p className="z-[10] mr-9 text-base font-medium leading-6 text-white">
+              Home
+            </p>
+          </Link>
+          <Link href="/">
+            <p className="z-[10] mr-9 text-base font-medium leading-6 text-white">
+              Templates
+            </p>
+          </Link>
+
+          <Link href="/">
+            <p className="z-[10] mr-9 text-base font-medium leading-6 text-white">
+              Pages
+            </p>
+          </Link>
+
+          <Link href="/">
+            <p className="z-[10] mr-9 text-base font-medium leading-6 text-white">
+              Element
+            </p>
+          </Link>
+
+          <Link href="/">
+            <p className="z-[10] mr-9 text-base font-medium leading-6 text-white">
+              Resources
+            </p>
+          </Link>
+        </div>
+
+        <Link href="/login">
+          <button className="h-9 w-24 py-6 rounded-3xl Button  text-white  text-primary  text-base font-bold cursor-pointer flex items-center justify-center">
+            Login
+          </button>
+        </Link>
       </nav>
     </div>
   );
--- my-app/src/components/dashboard/AdminPage.tsx
+++ my-app/src/components/dashboard/AdminPage.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import Link from "next/link";
 import { useDashboard } from "@/src/hooks/useDashboard";
-import NewUserData from "./NewUserData";
+import Sidebar from "../sidebar/Sidebar";
 
 export default function AdminPage() {
   const {
@@ -34,146 +33,118 @@ export default function AdminPage() {
     return <div>Error fetching user data: {fetchError.message}</div>;
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl mb-4">Dashboard</h1>
-
-      <div className="flex items-center mb-4">
-        <Link href="/user">
-          <button className="bg-blue-500 text-white py-2 px-4 rounded-md mr-4">
-            Create User
-          </button>
-        </Link>
-      </div>
-
-      <div className="mb-4">
-        <div>
-          <label>First Name:</label>
-          {editMode ? (
-            <input
-              type="text"
-              value={formData.firstName}
-              onChange={(e) =>
-                setFormData({ ...formData, firstName: e.target.value })
-              }
-              className="block w-full mb-2 p-2 border rounded"
-            />
-          ) : (
-            formData.firstName
-          )}
-        </div>
-        <div>
-          <label>Last Name:</label>{" "}
-          {editMode ? (
-            <input
-              type="text"
-              value={formData.lastName}
-              onChange={(e) =>
-                setFormData({ ...formData, lastName: e.target.value })
-              }
-              className="block w-full mb-2 p-2 border rounded"
-            />
-          ) : (
-            formData.lastName
-          )}
-        </div>
-
-        <div>
-          <label>Email:</label>
-          {formData.email}
+    <div className="flex min-h-screen h-full">
+      <Sidebar
+        handleEdit={handleEdit}
+        handleEditPassword={handleEditPassword}
+        handleEditEmail={handleEditEmail}
+        handleDeleteAccount={handleDeleteAccount}
+      />
+      <div className="flex-1 p-6 bg-gray-100">
+        <h1 className="text-2xl mb-4">Admin Detail</h1>
+        <div className="mb-4">
           <div>
-            <label>Password:</label>
-            {formData.password}
+            <label>First Name:</label>
+            {editMode ? (
+              <input
+                type="text"
+                value={formData.firstName}
+                onChange={(e) =>
+                  setFormData({ ...formData, firstName: e.target.value })
+                }
+                className="block w-full mb-2 p-2 border rounded"
+              />
+            ) : (
+              formData.firstName
+            )}
           </div>
-          {emailEditMode ? (
+          <div>
+            <label>Last Name:</label>{" "}
+            {editMode ? (
+              <input
+                type="text"
+                value={formData.lastName}
+                onChange={(e) =>
+                  setFormData({ ...formData, lastName: e.target.value })
+                }
+                className="block w-full mb-2 p-2 border rounded"
+              />
+            ) : (
+              formData.lastName
+            )}
+          </div>
+
+          <div>
+            <label>Email:</label>
+            {formData.email}
             <div>
-              <input
-                type="email"
-                value={formData.email}
-                className="block w-full mb-2 p-2 border rounded"
-                readOnly
-              />
-              <input
-                type="email"
-                value={newEmail}
-                onChange={(e) => setNewEmail(e.target.value)}
-                className="block w-full mb-2 p-2 border rounded"
-              />
-              <input
-                type="password"
-                value={currentPassword}
-                onChange={(e) => setCurrentPassword(e.target.value)}
-                placeholder="Current Password"
-                className="block w-full mb-2 p-2 border rounded"
-              />
+              <label>Password:</label>
+              {formData.password}
+            </div>
+            {emailEditMode && (
+              <div>
+                <input
+                  type="email"
+                  value={formData.email}
+                  className="block w-full mb-2 p-2 border rounded"
+                  readOnly
+                />
+                <input
+                  type="email"
+                  value={newEmail}
+                  onChange={(e) => setNewEmail(e.target.value)}
+                  className="block w-full mb-2 p-2 border rounded"
+                />
+                <input
+                  type="password"
+                  value={currentPassword}
+                  onChange={(e) => setCurrentPassword(e.target.value)}
+                  placeholder="Current Password"
+                  className="block w-full mb-2 p-2 border rounded"
+                />
+                <button
+                  onClick={handleSaveEmail}
+                  className="bg-blue-800 px-4 py-4 rounded-lg"
+                >
+                  Save Email
+                </button>
+              </div>
+            )}
+            {passwordEditMode && (
+              <div>
+                <input
+                  type="password"
+                  value={currentPassword}
+                  onChange={(e) => setCurrentPassword(e.target.value)}
+                  placeholder="Current Password"
+                  className="block w-full mb-2 p-2 border rounded"
+                />
+                <input
+                  type="password"
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  placeholder="New Password"
+                  className="block w-full mb-2 p-2 border rounded"
+                />
+                <button
+                  onClick={handleSavePassword}
+                  className="bg-blue-800 px-4 py-4 rounded-lg"
+                >
+                  Save Password
+                </button>
+              </div>
+            )}
+            {editMode && (
               <button
-                onClick={handleSaveEmail}
+                onClick={handleSave}
                 className="bg-blue-800 px-4 py-4 rounded-lg"
               >
-                Save Email
+                Save Changes
               </button>
-            </div>
-          ) : passwordEditMode ? (
-            <div>
-              <input
-                type="password"
-                value={currentPassword}
-                onChange={(e) => setCurrentPassword(e.target.value)}
-                placeholder="Current Password"
-                className="block w-full mb-2 p-2 border rounded"
-              />
-              <input
-                type="password"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-                placeholder="New Password"
-                className="block w-full mb-2 p-2 border rounded"
-              />
-              <button
-                onClick={handleSavePassword}
-                className="bg-blue-800 px-4 py-4 rounded-lg"
-              >
-                Save Password
-              </button>
-            </div>
-          ) : (
-            <div className="flex gap-5 px-4 py-4 cursor-pointer ">
-              <button
-                onClick={handleEdit}
-                className="bg-red-400 px-4 py-4 rounded-lg"
-              >
-                Edit Profile
-              </button>
-              <button
-                onClick={handleEditPassword}
-                className="bg-red-400 px-4 py-4 rounded-lg"
-              >
-                Edit Password
-              </button>
-              <button
-                onClick={handleEditEmail}
-                className="bg-red-400 px-4 py-4 rounded-lg"
-              >
-                Edit Email
-              </button>
-              <button
-                onClick={handleDeleteAccount}
-                className="bg-red-400 px-4 py-4 rounded-lg"
-              >
-                Delete Account
-              </button>
-            </div>
-          )}
-          {editMode && (
-            <button
-              onClick={handleSave}
-              className="bg-blue-800 px-4 py-4 rounded-lg"
-            >
-              Save Changes
-            </button>
-          )}
+            )}
+          </div>
         </div>
       </div>
-      <NewUserData />
     </div>
   );
 }
